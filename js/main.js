528870c3f1b6baecd69db55cc74623067a860135
shokhruh61 <shohruxmahmudov61@gmail.com>
2024-10-02 21:13:28 +0500

    feat:for tsikl

---
+++ js/main.js
@@ -0,0 +1,148 @@
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+/*
+for (let i = 1; i <= 20; i++) {
+  console.log(i);
+}
+
+let n = parseInt(prompt("Son kiriting:"));
+for (let i = 1; i <= n; i++) {
+  console.log(i + " kvadrati: " + i ** 2);
+}
+
+for (let i = 0; i <= 10; i += 2) {
+  console.log(i);
+}
+
+for (let i = 1; i <= 15; i += 2) {
+  console.log(i);
+}
+
+for (let i = 1; i <= 5; i++) {
+  let son = parseInt(prompt(i + "-sonni kiriting:"));
+  console.log(son);
+}
+
+for (let i = 15; i >= 5; i--) {
+  console.log(i);
+}
+
+let n8 = parseInt(prompt("Son kiriting:"));
+for (let i = 1; i <= n8; i++) {
+  if (n8 % i === 0) {
+    console.log(i);
+  }
+}
+
+let sum9 = 0;
+for (let i = 1; i <= 10; i++) {
+  let son = parseInt(prompt(i + "-sonni kiriting:"));
+  sum9 += son;
+}
+console.log("Yig'indi:", sum9);
+
+for (let i = 1; i <= 50; i++) {
+  if (i % 5 === 0) {
+    console.log(i);
+  }
+}
+
+for (let i = 100; i >= 1; i--) {
+  console.log(i);
+}
+
+let sum12 = 0;
+for (let i = 1; i <= 100; i++) {
+  sum12 += i;
+}
+console.log("Yig'indi:", sum12);
+
+let start13 = parseInt(prompt("Birinchi sonni kiriting:"));
+let end13 = parseInt(prompt("Ikkinchi sonni kiriting:"));
+for (let i = start13; i <= end13; i++) {
+  console.log(i);
+}
+
+let n14 = parseInt(prompt("Son kiriting:"));
+for (let i = 1; i <= n14; i++) {
+  if (n14 % i === 0) {
+    console.log(i);
+  }
+}
+
+for (let i = 1; i <= 20; i++) {
+  console.log(i + " kvadrati: " + i ** 2);
+}
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 7 === 0) {
+    console.log(i);
+  }
+}
+
+for (let i = 1; i <= 10; i++) {
+  for (let j = 1; j <= 10; j++) {
+    console.log(i + " x " + j + " = " + i * j);
+  }
+  console.log(); 
+}
+
+let n18 = parseInt(prompt("Son kiriting:"));
+let sum18 = 0;
+for (let i = 1; i <= n18; i++) {
+  if (n18 % i === 0) {
+    sum18 += i;
+    console.log(i);
+  }
+}
+console.log("Bo'linuvchilar yig'indisi:", sum18);
+
+let sum19 = 0;
+for (let i = 1; i <= 50; i += 2) {
+  sum19 += i;
+}
+console.log("Toq sonlar yig'indisi:", sum19);
+
+let start20 = parseInt(prompt("Birinchi sonni kiriting:"));
+let end20 = parseInt(prompt("Ikkinchi sonni kiriting:"));
+for (let i = start20; i <= end20; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+
+let sum21 = 0;
+for (let i = 1; i <= 100; i += 2) {
+  sum21 += i;
+}
+console.log("Toq sonlar yig'indisi:", sum21);
+
+let n22 = parseInt(prompt("Son kiriting:"));
+let faktorial = 1;
+for (let i = 1; i <= n22; i++) {
+  faktorial *= i;
+}
+console.log("Faktorial:", faktorial);
+for (let i = 1; i <= 50; i++) {
+  if (i % 3 !== 0 && i % 5 !== 0) {
+    console.log(i);
+  }
+}
+for (let i = 1; i <= 30; i++) {
+  if (i % 4 === 0 && i % 6 === 0) {
+    console.log(i);
+  }
+}
+
+let start25 = parseInt(prompt("Birinchi sonni kiriting:"));
+let end25 = parseInt(prompt("Ikkinchi sonni kiriting:"));
+let sum25 = 0;
+for (let i = start25; i <= end25; i++) {
+  if (i % 2 !== 0) {
+    sum25 += i;
+    console.log(i);
+  }
+}
+console.log("Toq sonlar yig'indisi:", sum25);
+*/
