8b67b1398275d971595d781c1575bb0c63285ef5
shokhruh61 <shohruxmahmudov61@gmail.com>
2024-10-03 13:59:35 +0500

    bug: for js

--- js/main.js
+++ js/main.js
@@ -1,94 +1,94 @@
-//1-masala
+// // 1-masala
 for (let i = 1; i <= 5; i++) {
   console.log(i);
 }
-// //2-masala
+// // 2-masala
 // for (let i = 1; i <= 20; i++) {
 //   console.log(i);
 // }
-// //3-masala
+// // 3-masala
 // let n = +prompt("Son kiriting:");
 // for (let i = 1; i <= n; i++) {
 //   console.log(i + " kvadrati: " + i ** 2);
 // }
-// //4-masala
+ ////4-masala
 // for (let i = 0; i <= 10; i += 2) {
 //   console.log(i);
 // }
-// //5-masala
+// // 5-masala
 // for (let i = 1; i <= 15; i += 2) {
 //   console.log(i);
 // }
-// //6-masala
+// // 6-masala
 // for (let i = 1; i <= 5; i++) {
 //   let son = +prompt(i + "-sonni kiriting:");
 //   console.log(son);
 // }
-// //7-masala
+// // 7-masala
 // for (let i = 15; i >= 5; i--) {
 //   console.log(i);
 // }
-//8-masala
+// // 8-masala
 // let n = +prompt("Son kiriting:");
 // for (let i = 1; i <= n; i++) {
 //   if (n8 % i === 0) {
 //     console.log(i);
 //   }
 // }
-//9-masala
+// // 9-masala
 // let sum = 0;
 // for (let i = 1; i <= 10; i++) {
 //   let son = +prompt(i + "-sonni kiriting:");
 //   sum += son;
 // }
 // console.log("Yig'indi:", sum);
-//10-masala
+// // 10-masala
 // for (let i = 1; i <= 50; i++) {
 //   if (i % 5 === 0) {
 //     console.log(i);
 //   }
 // }
-//11-masala
+// // 11-masala
 // for (let i = 100; i >= 1; i--) {
 //   console.log(i);
 // }
-//12-masala
+// // 12-masala
 // let sum = 0;
 // for (let i = 1; i <= 100; i++) {
 //   sum += i;
 // }
 // console.log("Yig'indi:", sum);
-//13-masala
+// // 13-masala
 // let start = +prompt("Birinchi sonni kiriting:");
 // let end = +prompt("Ikkinchi sonni kiriting:");
 // for (let i = start; i <= end; i++) {
 //   console.log(i);
 // }
-//14-masala
+// // 14-masala
 // let n = +prompt("Son kiriting:");
 // for (let i = 1; i <= n; i++) {
 //   if (n % i === 0) {
 //     console.log(i);
 //   }
 // }
-//15-masala
+// // 15-masala
 // for (let i = 1; i <= 20; i++) {
 //   console.log(i + " kvadrati: " + i ** 2);
 // }
-//16-masala
+// // 16-masala
 // for (let i = 1; i <= 100; i++) {
 //   if (i % 7 === 0) {
 //     console.log(i);
 //   }
 // }
-//17-masala
+// // 17-masala
 // for (let i = 1; i <= 10; i++) {
 //   for (let j = 1; j <= 10; j++) {
 //     console.log(i + " x " + j + " = " + i * j);
 //   }
 //   console.log();
 // }
-//18-masala
+// // 18-masala
 // let n = +prompt("Son kiriting:");
 // let sum = 0;
 // for (let i = 1; i <= n; i++) {
@@ -98,13 +98,13 @@ for (let i = 1; i <= 5; i++) {
 //   }
 // }
 // console.log("Bo'linuvchilar yig'indisi:", sum);
-//19-masala
+// // 19-masala
 // let sum = 0;
 // for (let i = 1; i <= 50; i += 2) {
 //   sum += i;
 // }
 // console.log("Toq sonlar yig'indisi:", sum);
-//20-masala
+// // 20-masala
 // let start = +prompt("Birinchi sonni kiriting:");
 // let end = +prompt("Ikkinchi sonni kiriting:");
 // for (let i = start; i <= end; i++) {
@@ -112,32 +112,32 @@ for (let i = 1; i <= 5; i++) {
 //     console.log(i);
 //   }
 // }
-//21-masala
+// // 21-masala
 // let sum = 0;
 // for (let i = 1; i <= 100; i += 2) {
 //   sum += i;
 // }
 // console.log("Toq sonlar yig'indisi:", sum);
-//22-masala
+// // 22-masala
 // let n = +prompt("Son kiriting:");
 // let faktorial = 1;
 // for (let i = 1; i <= n; i++) {
 //   faktorial *= i;
 // }
 // console.log("Faktorial:", faktorial);
-//23-masala
+// // 23-masala
 // for (let i = 1; i <= 50; i++) {
 //   if (i % 3 !== 0 && i % 5 !== 0) {
 //     console.log(i);
 //   }
 // }
-//24-masala
+// // 24-masala
 // for (let i = 1; i <= 30; i++) {
 //   if (i % 4 === 0 && i % 6 === 0) {
 //     console.log(i);
 //   }
 // }
-
+// // 25-masala
 // let start = +prompt("Birinchi sonni kiriting:");
 // let end = +prompt("Ikkinchi sonni kiriting:");
 // let sum = 0;
